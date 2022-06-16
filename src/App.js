78d8d05fd5c886acb80de4e4f892d4c2ab9185db
Brandon Stirling <brandonstirling1998@gmail.com>
2022-06-16 14:49:45 +1200

    Adding my name to the list

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 function App() {
   return (
     <>
-      <h1> Add your names to the list below. Make sure you do not remove anybody elses name. :D </h1>
+      <h1>
+        {" "}
+        Add your names to the list below. Make sure you do not remove anybody
+        elses name. :D{" "}
+      </h1>
       <ol>
         <li>Sebin Benjamin</li>
+        <li>Brandon S</li>
       </ol>
     </>
-    );
+  );
 }
 
 export default App;
