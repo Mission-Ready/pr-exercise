fd65454929502fbeeea2d4428db46d7d6de47be7
Sebin Benjamin <sebinbenjamin@gmail.com>
2022-06-16 13:15:40 +1200

    add names exercise

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1> Add your names to the list below. Make sure you do not remove anybody elses name. :D </h1>
+      <ol>
+        <li>Sebin Benjamin</li>
+      </ol>
+    </>
+    );
 }
 
 export default App;
