9aa2994e958795113bb1fa33d6f9300faa8e61d2
Sonja Fantom <sonja.fantom@gmail.com>
2022-06-16 15:24:46 +1200

    name changed

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ function App() {
     <>
       <h1> Add your names to the list below. Make sure you do not remove anybody elses name. :D </h1>
       <ol>
-        <li>Sebin Benjamin</li>
+      <li>Sebin Benjamin</li>
+        <li>Simon Li</li>
+        <li>David Bruner</li>
+        <li>Adan Millard</li>
+        <li>NEISH WAS HERE</li>
+        <li>Prashant Kumar</li>
         <li>Sonja Fantom</li>
       </ol>
     </>
