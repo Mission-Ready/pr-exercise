7b0cb0fdecf377b76a5f5caad29211ab3a43f7c3
Adan Millard <adanmillard@gmail.com>
2022-06-16 15:17:51 +1200

    added spaces

--- src/App.js
+++ src/App.js
@@ -2,9 +2,8 @@ function App() {
   return (
     <>
       <h1>
-        {" "}
         Add your names to the list below. Make sure you do not remove anybody
-        elses name. :D{" "}
+        elses name. :D
       </h1>
       <ol>
         <li>Sebin Benjamin</li>
