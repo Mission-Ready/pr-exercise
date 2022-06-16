f547c0fa7ad9e2ffe7fbecbee297f3c4a1f8e600
Brandon Stirling <brandonstirling1998@gmail.com>
2022-06-16 14:59:24 +1200

    updating the line my name is on

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ function App() {
   return (
     <>
       <h1>
-        {" "}
         Add your names to the list below. Make sure you do not remove anybody
-        elses name. :D{" "}
+        elses name. :D
       </h1>
       <ol>
         <li>Sebin Benjamin</li>
+
         <li>Brandon S</li>
       </ol>
     </>
